d7dd7a249f28897fb1e85a0e2456217c71348c5d
aahil n <nishad.a@northeastern.edu>
2024-08-04 10:40:14 -0400

    added nav & image changes

---
+++ src/components/instax.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useStore } from "@nanostores/react";
+import { direction } from "../store"; // Adjust the import as necessary
+import pfp from "../images/pfp.jpg";
+import left from "../images/left.png";
+import right from "../images/right.jpg";
+import "@fontsource/schoolbell";
+
+interface InstaxImage {
+  [key: string]: ImageMetadata;
+}
+
+const images: InstaxImage = {
+  home: pfp,
+  bio: pfp,
+  experience: left,
+  projects: right,
+  contact: pfp,
+};
+
+const Instax: React.FC = () => {
+  const dir = useStore(direction);
+  const pic = images[dir] || images.home;
+
+  return (
+    <div className="flex flex-col min-w-64 w-96 h-auto py-3 px-5 bg-white-500 text-black rounded-lg shadow-md hover:focus:outline-md focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75">
+      <img src={pic.src} alt="aahil" className="py-4" />
+      <div className="my-4 font-['Schoolbell'] flex flex-col justify-center text-center">
+        <span className="text-4xl font-bold">Aahil Nishad</span>
+        <span className="text-md font-semibold">
+          Northeastern '27, BSCS, Software Engineer
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Instax;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useStore } from "@nanostores/react";
+import { direction } from "../store.js";
+
+const Navbar: React.FC = () => {
+  function setDirection(dir: string) {
+    direction.set(dir);
+    console.log(direction.get());
+  }
+
+  const dir = useStore(direction);
+
+  return (
+    <nav className="flex flex-row w-full justify-around place-items-center">
+      <a
+        className="mt-8 p-3 text-2xl font-semibold group transition duration-400"
+        onMouseEnter={() => setDirection("bio")}
+        onMouseLeave={() => setDirection("home")}
+        href={`#${dir}`}
+      >
+        Bio
+        <div className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-amber-700"></div>
+      </a>
+      <a
+        className="mt-8 p-3 text-2xl font-semibold group transition duration-400"
+        onMouseEnter={() => setDirection("experience")}
+        onMouseLeave={() => setDirection("home")}
+        href={`#${direction.get()}`}
+      >
+        Experiences
+        <div className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-amber-700"></div>
+      </a>
+      <a
+        className="mt-8 p-3 text-2xl font-semibold group transition duration-400"
+        onMouseEnter={() => setDirection("projects")}
+        onMouseLeave={() => setDirection("home")}
+        href={`#${direction.get()}`}
+      >
+        Projects
+        <div className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-amber-700"></div>
+      </a>
+      <a
+        className="mt-8 p-3 text-2xl font-semibold group transition duration-400"
+        onMouseEnter={() => setDirection("contact")}
+        onMouseLeave={() => setDirection("home")}
+        href={`#${direction.get()}`}
+      >
+        Contact
+        <div className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-amber-700"></div>
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
